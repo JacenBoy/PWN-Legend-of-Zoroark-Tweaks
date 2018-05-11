550b5a6fa2fef55cae5653ade01f5fd7f4b3ae14
JacenBoy <jacenboy1@gmail.com>
2018-05-11 14:14:53 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         PWN Legend of Zoroark Tweaks
+// @namespace    http://jacenboy.ml/
+// @version      0.1
+// @description  Tweaks to make PWN's Legend of Zoroark theme better
+// @author       JacenBoy
+// @match        *forums.petalburgwoods.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    console.log("PWN Tweaks loaded");
+})();
